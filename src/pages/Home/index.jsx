bc7020e19fa84818cd3ec5ddf263583bbf6af0c3
Marcelo Siqueira <marcelojobxt7@gmail.com>
2024-02-13 18:01:20 -0300

    :hammer: feat: Created the home page and its functions

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,42 @@
+import { useState, useEffect } from 'react';
+
+import HomeContainer from './styles';
+import CardMovie from '../../components/CardMovie';
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 function Home() {
-    return (
-        <>
-            <h1>Home</h1>
-        </>
-    )
-};
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+    getTopRatedMovies(topRatedUrl);
+  }, []);
+
+  return (
+    <HomeContainer>
+      <h2 className="title">Melhores Filmes</h2>
+
+      <section className="moives-container">
+        {topMovies.length === 0 && <p>Carregando...</p>}
+
+        {topMovies.length > 0 && topMovies.map((movie) => (
+          <CardMovie
+            key={movie.id}
+            movie={movie}
+          />
+        ))}
+      </section>
+    </HomeContainer>
+  );
+}
 
 export default Home;
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const HomeContainer = styled.section`
+    .moives-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-evenly;
+    }
+`;
+
+export default HomeContainer;
