70340b63a874f1b7d0ce60853b5140451e42f27e
Marcelo Siqueira <marcelojobxt7@gmail.com>
2024-02-16 12:17:43 -0300

    :hammer: feat: Created the style and functionality of the movie view component

--- src/pages/Movie/index.jsx
+++ src/pages/Movie/index.jsx
@@ -1,7 +1,67 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router';
+import MovieContainer from './styles';
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+const imageUrl = import.meta.env.VITE_IMG;
+
 function Movie() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState();
+
+  const getMovie = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setMovie(data);
+  };
+
+  useEffect(() => {
+    const searchTheMovie = `${moviesURL}${id}?${apiKey}`;
+    getMovie(searchTheMovie);
+  }, []);
+
   return (
     <>
-      <h1>Movie</h1>
+      {movie && (
+      <MovieContainer>
+        <div className="info-image-movie">
+          <img src={imageUrl + movie.poster_path} alt={movie.title} />
+        </div>
+        <div className="info-movie-container">
+          <h1 className="title-movie">{movie.title}</h1>
+          <p className="about-movie">{movie.overview}</p>
+          <div className="genres-container">
+            <h3>Categorias:</h3>
+            <div className="genres">
+              {movie.genres.map((genre) => (
+                <span key={genre.id}>{genre.name}</span>
+              ))}
+            </div>
+          </div>
+          <div className="data-movie">
+            <h3>Data de Lançamento:</h3>
+            <span>{movie.release_date}</span>
+          </div>
+          <div className="companies-movie-container">
+            <h3>Produzido por: </h3>
+            <div className="companies-movie">
+              {movie.production_companies.map((companies) => (
+                <a
+                  key={companies.id}
+                  className="companie"
+                  href={`https://www.google.com/search?q=${companies.name}`}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {companies.name}
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
+      </MovieContainer>
+      )}
     </>
   );
 }
---
+++ src/pages/Movie/styles.js
@@ -0,0 +1,85 @@
+import styled from 'styled-components';
+import common from '../../styles/themes/common';
+
+const MovieContainer = styled.section`
+    margin: 2rem;
+    display: flex;
+    gap: 2rem;
+
+    .info-movie-container {
+        max-width: 800px;
+
+        .title-movie {
+            font-size: 38px;
+            letter-spacing: 1.6px;
+        }
+
+        .about-movie {
+            padding: 0.6rem 0 0.6rem 0;
+            font-size: 18px;
+        }
+
+        .genres-container {
+            h3 {
+                font-size: 26px;
+            }
+
+            .genres {
+                display: flex;
+                flex-direction: row;
+                gap: 1rem;
+                padding: 0.6rem 0 0.6rem 0;
+
+                span {
+                    background-color: #536FED;
+                    text-align: center;
+                    width: 120px;
+                    padding: 6px;
+                    border-radius: ${common.borderRadius};
+                    cursor: pointer;
+                    transition: 0.2s;
+                }
+
+                span:hover {
+                        background-color: #2741b3;
+                    }
+
+            }
+        }
+
+        .data-movie {
+            padding: 0.6rem 0 0.6rem 0;
+
+            h3 {
+                font-size: 26px;
+            }
+
+            span {
+                font-size: 20px;
+                letter-spacing: 2px;
+
+            }
+        }
+
+        .companies-movie-container {
+            h3 {
+                font-size: 26px;
+            }
+
+            .companies-movie {
+                font-size: 18px;
+                font-weight: 600;
+                display: flex;
+                flex-direction: row;
+                gap: 1rem;
+                cursor: pointer;
+            }
+
+            .companie:hover {
+                color: #536FED;
+            }
+        }
+    }
+`;
+
+export default MovieContainer;
